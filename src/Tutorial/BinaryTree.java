b0bf22b27afb6b3f236f227ba185b0786b20e452
thebigsasha <sashathethird@gmail.com>
2020-11-30 13:47:51 -0500

    It might work (?) I hope

--- src/Tutorial/BinaryTree.java
+++ src/Tutorial/BinaryTree.java
@@ -37,6 +37,7 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
         while(temp.getLeft() != null){
             temp = temp.getLeft();
         }
+
         return temp.getData();
     }
 
@@ -167,19 +168,22 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
      */
     @Override
     public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            private final Stack<E> inOrder = reverseOrder();
-            @Override
-            public boolean hasNext() {
+        return new BTreeIterator();
+    }
 
-                return !inOrder.isEmpty();
-            }
+    class BTreeIterator implements Iterator<E>{
+        private final Stack<E> inOrder = reverseOrder();
+        private TreeNode<E> root = BinaryTree.this.root.left;
+        @Override
+        public boolean hasNext() {
 
-            @Override
-            public E next() {
-                return inOrder.pop();
-            }
-        };
+            return !inOrder.isEmpty();
+        }
+
+        @Override
+        public E next() {
+            return inOrder.pop();
+        }
     }
 
     public void delete(E data){
--- src/Tutorial/Treap.java
+++ src/Tutorial/Treap.java
@@ -1,5 +1,8 @@
 package Tutorial;
 
+import com.sun.source.tree.BinaryTree;
+
+import java.util.Arrays;
 import java.util.Random;
 
 class TreapNode<E extends Comparable<E>>
@@ -43,7 +46,10 @@ class TreapNode<E extends Comparable<E>>
 
     public TreapNode(E data) {
         this.data = data;
-        this.priority = new Random().nextInt(100);
+        if(data instanceof PocketMonster){
+            this.priority = ((PocketMonster) data).getDamage(); //THIS IS LIKE IN A3 WHERE PRIORITY IS A FIELD OF THE OBJECT
+        }
+        this.priority = new Random().nextInt();                 //THIS IS LIKE A NORMAL TREAP, WHERE PRIORITY IS RANDOM
     }
 }
 
@@ -53,15 +59,50 @@ class Treap<E extends Comparable<E>>
 
 
     public TreapNode<E> rotateLeft(TreapNode<E> root){
-        return null;
+        TreapNode<E> Right = root.right;
+        TreapNode<E> Left = root.right.left;
+
+        Right.left = root;
+        root.right = Left;
+
+        this.root = Right;
+        return Right;
     }
 
 
     public TreapNode<E> rotateRight(TreapNode<E> root) {
-        return null;
+        TreapNode<E> left = root.left;
+        TreapNode<E> right = root.left.right;
+
+        left.right = root;
+        root.left = right;
+
+        this.root = left;
+        return left;
     }
 
     public TreapNode<E> add(TreapNode<E> root, E data) {
+
+        if(root == null){
+            return new TreapNode<>(data);
+        }
+
+        //BST Check, lefty larger or lefty smaller depending
+        if(data.compareTo(root.data) < 0){
+            root.left = add(root.left, data);
+
+            //THE TREAP PART, THE FUN PART, THIS IS WHERE THE FUN BEGINS
+            if(root.left != null && root.left.priority > root.getPriority()){   //In A3, priority is defined as one of the fields of the dog, that is, how long its been in the shelter
+                root = rotateRight(root);
+            }
+        }else{
+            root.right = add(root.right, data);
+
+            if(root.right != null && root.right.priority > root.getPriority()){
+                root = rotateLeft(root);
+            }
+        }
+
         return root;
     }
 
@@ -73,30 +114,77 @@ class Treap<E extends Comparable<E>>
         root = add(root, key);
     }
 
-    public TreapNode<E> delete(TreapNode<E> root, E key)
-    {
-        return null;
+    public TreapNode<E> delete(TreapNode<E> root, E key) {
+        if(root == null){
+            return null;
+        }
+
+        //Here we search for what we are removing
+        if(key.compareTo(root.data) < 0){
+            root.left = delete(root.left,key);
+        } else if(key.compareTo(root.data) > 0){
+            root.right = delete(root.right,key);
+        }
+
+        //Here we have found the thing to remove:
+        else{
+            //Base case, easy to kill when they have no children who will miss them
+            if(root.left == null && root.right == null){
+                root = null;
+            }
+
+            else if(root.left != null && root.right != null){
+
+                if(root.left.priority < root.right.getPriority()){
+                    root = rotateLeft(root);
+
+                    root.left = delete(root.left, key);
+                }else{
+                    root = rotateRight(root);
+
+                    root.right = delete(root.right,key);
+                }
+            }
+
+            else{
+                TreapNode<E> child;
+                if(root.left != null) {
+                     child = root.left;
+                }else{
+                    child = root.right;
+                }
+                root = child;
+            }
+        }
+        return root;
     }
 
     public static void main(String[] args)
     {
         // Treap keys
-        int[] keys = { 5, 2, 1, 4, 9, 8, 10 };
+        int[] keys = { 5, 2, 1, 4, 9, 8, 10, 11, 12, 13, 14, 15, -4, 124, -444 };
+
+        Arrays.sort(keys);
 
         // construct a Treap
         Treap<Integer> tr = new Treap<>();
-        for (int key: keys)
+        Tutorial.BinaryTree<Integer> tree = new Tutorial.BinaryTree<>();
+        for (int key: keys) {
             tr.add(key);
+            tree.add(key);
+        }
 
-        System.out.println("Constructed Treap:\n\n");
 
-        System.out.println("\nDeleting node 1:\n\n");
         tr.delete(1);
 
-        System.out.println("\nDeleting node 5:\n\n");
         tr.delete(5);
 
-        System.out.println("\nDeleting node 9:\n\n");
         tr.delete(9);
+
+        tr.delete(keys[7]);
+
+        tr.delete(keys[6]);
+
+        tr.delete(keys[5]);
     }
 }
